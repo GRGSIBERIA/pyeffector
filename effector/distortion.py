ba4b9b6509e3b322e398cb2f3d7c9e49abe55cbe
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-06-14 23:33:14 +0900

    np.clipのjitが実装されていないのでabsなどに置き換える

--- effector/distortion.py
+++ effector/distortion.py
@@ -1,17 +1,18 @@
 import numpy as np
 from numba import jit
 
-@jit
+@jit("void(f8[:], f8[:], f8, f8)")
 def hard_clipping(indata: np.ndarray, outdata: np.ndarray, amplitude=5.0, level=0.8):
-    outdata[:] = np.clip(indata * amplitude, -1.0, 1.0) * level
+    length = np.abs(indata * amplitude)
+    outdata[:] = np.where(length > 1.0, indata * np.sign(indata), length * level)
 
-@jit
+@jit("void(f8[:], f8[:], f8, f8, f8)")
 def soft_clipping(indata: np.ndarray, outdata: np.ndarray, amplitude=5.0, level=0.8, quarity=0.3):
     d = np.arctan(indata) / (np.pi / 2.0)
     d[:] = np.where(d > 0, amplitude * d, amplitude * d * quarity)
-    outdata[:] = np.clip(d, -1.0, 1.0)
+    outdata[:] = np.where(np.abs(d) > 1.0, np.sign(d) * 1.0, d * level)
 
-@jit
+@jit("void(f8[:], f8[:], f8, f8)")
 def compressor(indata: np.ndarray, outdata: np.ndarray, threshold=0.1, ratio=2.0):
     diff = (np.abs(indata) - threshold) / ratio
     outdata[:] = np.where(np.abs(indata) < threshold, 
