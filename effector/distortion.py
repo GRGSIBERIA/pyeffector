783a3417b47970da826c4634452a7ca5c2173458
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-06-14 23:18:52 +0900

    vectorizeではなくjitで動かしつつ、コンプレッサを追加

--- effector/distortion.py
+++ effector/distortion.py
@@ -1,12 +1,21 @@
 import numpy as np
-from numba import vectorize, void, float64
+from numba import jit
 
-@vectorize([void(float64[:], float64[:], float64, float64)])
+@jit
 def hard_clipping(indata: np.ndarray, outdata: np.ndarray, amplitude=5.0, level=0.8):
     outdata[:] = np.clip(indata * amplitude, -1.0, 1.0) * level
 
-@vectorize([void(float64[:], float64[:], float64, float64, float64)])
+@jit
 def soft_clipping(indata: np.ndarray, outdata: np.ndarray, amplitude=5.0, level=0.8, quarity=0.3):
     d = np.arctan(indata) / (np.pi / 2.0)
     d[:] = np.where(d > 0, amplitude * d, amplitude * d * quarity)
     outdata[:] = np.clip(d, -1.0, 1.0)
+
+@jit
+def compressor(indata: np.ndarray, outdata: np.ndarray, threshold=0.1, ratio=2.0):
+    diff = (np.abs(indata) - threshold) / ratio
+    outdata[:] = np.where(np.abs(indata) < threshold, 
+        indata, 
+        np.where(indata < 0.0, 
+            -(threshold + diff), 
+            threshold + diff))
