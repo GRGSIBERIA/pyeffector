c8acb59391821c8d10c5fcc5fc01903fd55c7549
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-06-22 00:22:22 +0900

    アイコンを作る、値を入れただけ

--- NoPythonEffector/NoPythonEffector/Effector.hpp
+++ NoPythonEffector/NoPythonEffector/Effector.hpp
@@ -2,10 +2,48 @@
 #include <mutex>
 #include <math.h>
 #include <numbers>
+#include <Siv3D.hpp>
 #include "TinyASIO/TinyASIO.hpp"
 
 namespace effector
 {
+	class Icon
+	{
+		Vec2 center;
+		float radius = 10.0;
+		float volume = 0.0;
+		float movedup = 8.0;
+		float dot_radius = 2.0;
+
+		const float half_angle = 165.0;
+
+	public:
+		Icon(const Vec2& _center, const float _radius, const float _volume, const float _movedup, const float _dot_radius)
+			: radius(_radius), center(_center), volume(_volume), movedup(_movedup), dot_radius(_dot_radius)
+		{
+
+		}
+
+		void update()
+		{
+			const float sign = (volume > 0.0) - (volume < 0.0);	// 符号を取る
+			volume = sign < 0.0 ? 0.0 : (volume > 1.0 ? 1.0 : volume);	// [0,1]の値を取らせる
+		}
+
+		void draw()
+		{
+			const auto tsumami = Circle(center, radius).draw(Palette::Darkgray);
+
+			const auto identity = Vec2{ 1.0, 0.0 }.rotate(2.0 * half_angle * volume - half_angle);
+
+			Circle(center + identity * movedup, dot_radius).draw(Palette::Gray);
+		}
+
+		void setVolume(const float _volume) { volume = _volume; }
+
+		const float getVolume() const { return volume; }
+	};
+
 	class Effector
 	{
 	public:
